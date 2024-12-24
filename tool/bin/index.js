dd1bdcb98ceffbb79bb2bc435777aad458263b06
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2024-12-24 17:06:35 +0530

    Updated the CLI error managemant, customize color, and package path management

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 const arg = require('arg');
+const chalk = require('chalk');
+const path = require('path');
+
 try {
     const args = arg({
         '--start': Boolean,
         '--build': Boolean,
     });
+
     if (args['--start']) {
-        console.log('starting the app...');
+        const pkg = require(path.join(process.cwd(), 'package.json'));
+        //TODO: do something with the package
+        console.log(chalk.bgCyanBright('Starting the app...'));
     }
 } catch (err) {
-    console.log(err.message);
+    console.log(chalk.yellow(err.message));
+    console.log();
+    usage();
+}
+
+function usage() {
+    console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
 }
