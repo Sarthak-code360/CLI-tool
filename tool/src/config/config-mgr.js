2fa98faaecfdcfa7fa27cf60ad615c32eb972c1f
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2024-12-26 14:24:23 +0530

    Updated new features

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -4,6 +4,8 @@ const configLoader = cosmiconfigSync('tool');
 
 module.exports = function getConfig() {
     const result = configLoader.search(process.cwd());
+    console.log('Searching configuration in:', process.cwd());
+
     if (!result) {
         console.log(chalk.yellow('Could not find configuration, using default'));
         return { port: 1234 };
